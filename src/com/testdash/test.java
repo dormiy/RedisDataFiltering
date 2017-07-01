d31a2a2780bbcb19f9e4b92f8486bf55a496a2e1
PingWu <ping_wuu@163.com>
2017-07-01 15:50:40 +0800

    initial model on factory works... keep the comboBox event handling

--- src/com/testdash/test.java
+++ src/com/testdash/test.java
@@ -1,22 +1,36 @@
 package com.testdash;
 
+import impl.org.controlsfx.skin.CheckComboBoxSkin;
 import javafx.application.Application;
 import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.collections.FXCollections;
+import javafx.collections.ListChangeListener;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
+import javafx.event.Event;
 import javafx.event.EventHandler;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
+import javafx.scene.control.Skin;
+import javafx.scene.input.ContextMenuEvent;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.GridPane;
 
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
-import javax.swing.text.html.Option;
-import java.awt.event.KeyEvent;
 import java.util.ArrayList;
 
+import org.controlsfx.ControlsFXSample;
+import org.controlsfx.control.CheckComboBox;
+import org.controlsfx.control.IndexedCheckModel;
+import org.controlsfx.samples.Utils;
 
 /**
  * Created by wuping on 6/26/2017.
@@ -25,59 +39,88 @@ public class test extends Application{
 
     static RedisHandling redis;
     static OptionFilteringService optionFilteringService;
+    static boolean initialIndicator =true;
 
     @Override
     public void start(Stage primaryStage) throws Exception {
 
+        int comboWidth = 150;
+
+
+
+
 
         //define the content;
         Label factoryLabel = new Label("Factory");
-        ComboBox factoryComboBox = new ComboBox();
+        //ComboBox factoryComboBox = new ComboBox();
+        final CheckComboBox<String> factoryComboBox = new CheckComboBox<String>();
+        //factoryComboBox.setPrefWidth (comboWidth);
+
 
         Label customerLabel = new Label("Customer");
-        ComboBox customerComboBox = new ComboBox();
+        final CheckComboBox<String> customerComboBox = new CheckComboBox<String>();
+        customerComboBox.setPrefWidth (comboWidth);
 
         Label productLabel = new Label("Product");
         ComboBox productComboBox = new ComboBox();
+        productComboBox.setPrefWidth (comboWidth);
 
         Label packageLabel = new Label("Package");
         ComboBox packageComboBox = new ComboBox();
+        packageComboBox.setPrefWidth (comboWidth);
 
         Label deviceLabel = new Label("Device");
         ComboBox deviceComboBox = new ComboBox();
+        deviceComboBox.setPrefWidth (comboWidth);
 
         Label partnoLabel = new Label("Partno");
         ComboBox partnoComboBox = new ComboBox();
+        partnoComboBox.setPrefWidth (comboWidth);
 
         Label stageLabel = new Label("Stage");
         ComboBox stageComboBox = new ComboBox();
+        stageComboBox.setPrefWidth (comboWidth);
 
         Label operationLabel = new Label("Operation");
         ComboBox operationComboBox = new ComboBox();
+        operationComboBox.setPrefWidth (comboWidth);
 
         Label testCodeLabel = new Label("Testcode");
         ComboBox testCodeComboBox = new ComboBox();
+        testCodeComboBox.setPrefWidth (comboWidth);
 
         Label testProgramLabel = new Label("TestProgram");
         ComboBox testProgramComboBox = new ComboBox();
+        testProgramComboBox.setPrefWidth (comboWidth);
 
         Label programRevisionLabel = new Label("ProgramRevision");
         ComboBox programRevisionComboBox = new ComboBox();
+        programRevisionComboBox.setPrefWidth (comboWidth);
 
         Label camTypeLabel = new Label("CamType");
         ComboBox camTypeComboBox = new ComboBox();
+        camTypeComboBox.setPrefWidth (comboWidth);
 
         Label platformLabel = new Label("Platform");
         ComboBox platformComboBox = new ComboBox();
+        platformComboBox.setPrefWidth (comboWidth);
 
         Button btn=new Button("Submit");
 
 
+
         //add the contents into the grid pan
         GridPane grid = new GridPane();
+        grid.setAlignment (Pos.CENTER);
+
+        grid.setHgap (40);
+        grid.setVgap (5);
+        grid.setPadding (new Insets (30,0,0,0));
+
 
         grid.add(factoryLabel,0,0);
         grid.add(factoryComboBox,1,0);
+        //grid.add(factoryComboBox,1,0,20,10);
 
         grid.add(customerLabel,0,1);
         grid.add(customerComboBox,1,1);
@@ -115,8 +158,9 @@ public class test extends Application{
         grid.add(platformLabel,0,12);
         grid.add(platformComboBox,1,12);
 
-        grid.add(btn, 1,13);
 
+        grid.add(btn, 1,13);
+        btn.setAlignment (Pos.BOTTOM_RIGHT);
 
         btn.setOnAction(new EventHandler<ActionEvent>() {
             @Override
@@ -126,35 +170,72 @@ public class test extends Application{
         });
 
 
-       factoryComboBox.setOnShowing((e) -> {
+        factoryComboBox.setOnMouseEntered (new EventHandler<MouseEvent> () {
+            @Override
+            public void handle(MouseEvent event) {
+                ArrayList result;
+                result = optionFilteringService.getMenu ("Factory", initialIndicator);
+                for (int i = 0; i < result.size (); i++) {
+                    factoryComboBox.getItems ().add (result.get (i).toString ());
+                }
+                initialIndicator = false;
+                //System.out.println(factoryComboBox.getSelectionModel().getSelectedItem());
+            }
+        });
+        
+        factoryComboBox.getCheckModel().getCheckedItems().addListener (new ListChangeListener<String>() {
+            public void onChanged(ListChangeListener.Change<? extends String> c) {
+                System.out.println(factoryComboBox.getCheckModel().getCheckedItems());
+            }
+        });
+
+
+
+
+
+       /*factoryComboBox.setOnShowing((e) -> {
            factoryComboBox.getItems().removeAll();
            factoryComboBox.getItems().clear();
-           ArrayList result = optionFilteringService.getMenu("Factory");
 
-           for (int i = 0; i < result.size(); i++) {
-               factoryComboBox.getItems().add(result.get(i));
+           ArrayList result;
+           if(initialIndicator) {
+               result = optionFilteringService.getMenu ("Factory", initialIndicator);
+               for (int i = 0; i < result.size (); i++) {
+                   factoryComboBox.getItems ().add (result.get (i));
+               }
+               initialIndicator = false;
+           }
 
+           else {
+               result = optionFilteringService.getMenu("Factory",initialIndicator);
+               for (int i = 0; i < result.size(); i++) {
+                   factoryComboBox.getItems().add(result.get(i));
+               }
            }
            //System.out.println(factoryComboBox.getSelectionModel().getSelectedItem());
        });
 
+*/
 
-       factoryComboBox.setOnAction((e) -> {
 
-            System.out.println(factoryComboBox.getSelectionModel().getSelectedItem());
-        });
+
 
         //put the grid pan into a Vbox
         VBox root = new VBox();
+
+
+
+
         root.getChildren().add(grid);
-        Scene scene = new Scene(root, 400, 600);
+        Scene scene = new Scene(root, 400, 500);
         primaryStage.setTitle("Option Filtering");
         primaryStage.setScene(scene);
         primaryStage.show();
     }
 
     public static void main(String[] args){
-        String redisServer = "redis://TDnI123!@aurora:6379";
+        //String redisServer = "redis://TDnI123!@aurora:6379";
+        String redisServer = "redis://localhost:6379";
         redis= new RedisHandling(redisServer);
         redis.redisConnect();
         optionFilteringService = new OptionFilteringService();
