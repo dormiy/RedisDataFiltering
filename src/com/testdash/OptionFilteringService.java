b18edf6362a220e25453047894f49499fe86950a
PingWu <ping_wuu@163.com>
2017-07-01 21:59:36 +0800

    add drop down menu result handling for Factory

--- src/com/testdash/OptionFilteringService.java
+++ src/com/testdash/OptionFilteringService.java
@@ -1,6 +1,5 @@
 package com.testdash;
 
-import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Random;
 
@@ -9,9 +8,8 @@ public class OptionFilteringService {
 
 
     static int userKey;
-    static ArrayList secondKeyIn = new ArrayList();
+    static ArrayList optionInput = new ArrayList();
     static RedisHandling redis;
-    static boolean initialIndicator=true;
 
 
     public void userCreation(){
@@ -21,8 +19,8 @@ public class OptionFilteringService {
     }
 
 
-    public ArrayList getMenu(String nameMenu) {
-        secondKeyIn.clear();
+    public ArrayList getMenu(String nameMenu, boolean initialIndicator) {
+        optionInput.clear();
         String selectSet = "user" + Integer.toString(userKey);
         String selectSetTemp = "user" + Integer.toString(userKey) + "-temp";
         redis.commands.del(selectSetTemp);
@@ -31,14 +29,14 @@ public class OptionFilteringService {
         if (initialIndicator) {
             int count = 0;
             for (String x : redis.commands.smembers("option" + ":" + nameMenu)) {
-                secondKeyIn.add(x);
+                optionInput.add(x);
             }
             // initialIndicator=false;
         } else {
             for (String s : redis.commands.smembers("option" + ":" + nameMenu)) {
                 Long sinterResult = redis.commands.sinterstore(selectSetTemp, selectSet, "ATE" + ":" + nameMenu + ":" + s);
                 if (sinterResult != 0) {
-                    secondKeyIn.add(s);
+                    optionInput.add(s);
                 }
             }
         }
@@ -46,8 +44,52 @@ public class OptionFilteringService {
             System.out.println(i + ":" + secondKeyIn.get(i));
         }*/
 
-        return secondKeyIn;
+
+
+        return optionInput;
 
 
     }
+
+    public void selecionHandle(String nameMenu, ArrayList<String> result, boolean initialIndicator){
+        String selectSet = "user-"+Integer.toString(userKey);
+        String selectSetTemp = "user-"+Integer.toString(userKey)+"-temp";
+        String selectSetColumn="user-"+Integer.toString(userKey)+nameMenu;
+        redis.commands.del(selectSetTemp);
+
+        //store second menu result into an temp union
+        for(int i=0; i<result.size ();i++){
+            String s ="ATE:"+nameMenu+":"+result.get(i);
+            redis.commands.sunionstore(selectSetTemp, selectSetTemp,s);
+            redis.commands.sunionstore(selectSetColumn, selectSetColumn,s);
+        }
+
+        //initiate the user selection union when 1st time running
+        //for 2nd onwards running, sinter the current selection with available union;
+        if(initialIndicator){
+            redis.commands.sunionstore(selectSet,selectSetTemp);
+            initialIndicator=false;
+        }
+        else{
+            redis.commands.sinterstore(selectSet,selectSet,selectSetTemp);
+        }
+    }
+
+
+    public ArrayList resultBuild(String result) {
+        ArrayList<String> string = new ArrayList ();
+        StringBuilder s = new StringBuilder ();
+
+        for (int j = 0; j < result.length (); j++) {
+            if (result.charAt (j) == 32 || result.charAt (j) == 91) {
+            } else if (result.charAt (j) == 44 || result.charAt (j) == 93) {
+                string.add (s.toString ());
+                s.setLength (0);
+            } else
+                s.append (Character.toString (result.charAt (j)));
+        }
+        return string;
+    }
+
+
 }
